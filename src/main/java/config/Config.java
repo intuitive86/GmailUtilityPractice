3fdb639b324ad53784ddc34e55d551a1610d63b9
mikel tan <mikel.tan@clearcapital.com>
2020-07-01 21:45:21 -0700

    first commit

---
+++ src/main/java/config/Config.java
@@ -0,0 +1,9 @@
+package config;
+
+public class Config {
+
+    public static final String CLIENT_ID = "YOUR_CLIENT_ID.apps.googleusercontent.com";
+    public static final String TOKEN_URI= "https://oauth2.googleapis.com/token";
+    public static final String CLIENT_SECRET = "YOU_CLIENT_SECRET";
+    public static final String REFRESH_TOKEN = "YOUR_REFRESH_TOKEN";
+}
